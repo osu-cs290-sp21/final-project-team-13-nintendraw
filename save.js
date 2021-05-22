1016600d611b5ac2817cc870f91c684bb591e488
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-05-22 00:57:21 -0700

    save menu orientation

--- save.js
+++ save.js
@@ -7,7 +7,9 @@ console.log(saveButton)
 saveButton.addEventListener('click', function() {
     var saveBack = document.getElementById("save-backdrop")
     var saveMenu = document.getElementById("save-menu-container")
+    var saveItems = document.getElementById("save-menu-items")
 
     saveMenu.className = "visible"
     saveBack.className = "visible"
+    saveItems.className = "visible"
 })
