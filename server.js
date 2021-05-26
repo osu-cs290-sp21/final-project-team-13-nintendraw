4fd1ebd29e68df640363fb77279b7b1c11239024
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-05-26 00:22:10 -0700

    working on hoseing handlebar files

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(logger)
 
 app.use(express.static("public"))
 
+app.get("/home", function(req, res, next) {
+    res.status(200).render("drawHome")
+})
+
 app.listen(port, function() {
     console.log("== Server is listening on port: ", port)
 })
