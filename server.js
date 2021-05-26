7d28818abe04901b4c721649e06a47c5bb5cd89a
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-05-26 16:45:16 -0700

    install express and handlebars

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express")
 var logger = require("./logger.js")
+var exphbs = require('express-handlebars')
 
 var app = express()
 var port = 3001
@@ -8,9 +9,9 @@ app.use(logger)
 
 app.use(express.static("public"))
 
-app.get("/home", function(req, res, next) {
-    res.status(200).render("drawHome")
-})
+// app.get("/home", function(req, res, next) {
+//     res.status(200).render("drawHome")
+// })
 
 app.listen(port, function() {
     console.log("== Server is listening on port: ", port)
