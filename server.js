91178d6710de223fd9278bcf28bb354a8c4e221d
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-05-26 20:07:48 -0700

    got handlebars w/ css working, just need to fix js

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
 var path = require("path")
 var express = require("express")
-var logger = require("./logger.js")
 var exphbs = require('express-handlebars')
 
 var app = express()
-var port = 3001
+var port = 8000
 
-app.use(logger)
-app.engine("handlebars", exphbs({defaultLayout: null}))
+app.use(express.static("public"))
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "menuBar",
+    layoutDir: path.join(__dirname, "views/layouts")
+}))
+
 app.set("view engine", "handlebars")
 
-// app.use(express.static("public"))
-
 app.get("/", function(req, res, next) {
-    res.status(200).render("drawHome")
+    res.status(200).render("home", {
+        title: "Home",
+        css: "style",
+        js: "index"
+    })
+})
+
+app.get("/gallery", function(req, res, next) {
+    res.status(200).render("gallery", {
+        title: "Gallery",
+        css: "style"
+    })
+})
+
+app.get("/howToPlay", function(req, res, next) {
+    res.status(200).render("howToPlay", {
+        title: "How to Play",
+        css: "style"
+    })
+})
+
+app.get("*", function(req, res, next) {
+    res.status(200).render("404", {
+        title: "404 Error",
+        css: "style"
+    })
 })
 
 app.listen(port, function() {
