53f27b89bbd3857ed7bc9d98cfe0427efe166349
Cao Duy Nguyen <nguyecao@oregonstate.edu>
2021-06-06 02:05:15 -0700

    commit changes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var path = require("path")
 var express = require("express")
 var exphbs = require('express-handlebars')
+var fs = require('fs')
+
+var drawingData = require('./drawingData.json')
+var recentData = []
+for (var i = 0; i < 5; i++) {
+    recentData.push(drawingData[drawingData.length-1-i])
+}
+
 
 var app = express()
 var port = 8000
@@ -14,36 +22,40 @@ app.engine("handlebars", exphbs({
 
 app.set("view engine", "handlebars")
 
-app.get(["/", "/home"], function(req, res, next) {
+app.post(["/", "/home"], function (req, res, next) {
+})
+
+app.get(["/", "/home"], function (req, res, next) {
     res.status(200).render("home", {
         title: "Home",
         css: "style",
-        js: "index"
+        js: "index",
+        card: recentData
     })
 })
 
-app.get("/gallery", function(req, res, next) {
+app.get("/gallery", function (req, res, next) {
     res.status(200).render("gallery", {
         title: "Gallery",
-        js:'search',
-        css: "style"
+        css: "style",
+        card: drawingData
     })
 })
 
-app.get("/howToPlay", function(req, res, next) {
+app.get("/howToPlay", function (req, res, next) {
     res.status(200).render("howToPlay", {
         title: "How to Play",
         css: "style"
     })
 })
 
-app.get("*", function(req, res, next) {
+app.get("*", function (req, res, next) {
     res.status(200).render("404", {
         title: "404 Error",
         css: "style"
     })
 })
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("== Server is listening on port: ", port)
 })
